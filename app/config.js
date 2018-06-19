3e1a20fee2da6d728f26133a591c8e951a4327ba
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-06-19 08:19:34 +0100

    Modified header for DWP Digital

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'DWP Design Examples',
+  serviceName: 'Design Examples',
 
   // Default port that prototype runs on
   port: '3000',

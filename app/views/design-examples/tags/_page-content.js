4547256a46ee8dc381dcd0b44f583795e95183e5
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-06-19 08:47:10 +0100

    Updated stillToDo and howToUse

--- app/views/design-examples/tags/_page-content.js
+++ app/views/design-examples/tags/_page-content.js
@@ -23,7 +23,17 @@ module.exports = {
       To follow.
     </p>
   `,
-  howToUse: ``,
+  howToUse: `
+    <p>
+      Add the class <code class="language-html">app-c-tag</code> to any tag such 
+      as a span. Add any of the additional modifiers to change the colour.
+    </p>
+    <ul class="list list-bullet">
+      <li><code class="language-html">app-c-tag--success</code></li>
+      <li><code class="language-html">app-c-tag--warning</code></li>
+      <li><code class="language-html">app-c-tag--danger</code></li>
+    </ul>
+  `,
   research: `
     <ul class="list list-bullet">
       <li>User type: Citizens and agents</li>
@@ -35,9 +45,13 @@ module.exports = {
       <li>Manage Bereavement Support Payment</li>
     </ul>
   `,
-  stillToDo: ``,
+  stillToDo: `
+    <p>
+      More work is needed to work out what colours need to be available.
+    </p>
+  `,
   discussion: {
-    linkText: 'Discuss declarions on GitHub',
+    linkText: 'Discuss tags on GitHub',
     linkURL: 'https://github.com/dwp/design-examples/issues/9'
   }
 }
